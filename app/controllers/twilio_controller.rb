5106b12f43a213095b980faec525e9b90acc8779
Gary Bethea <betheagary@gmail.com>
2014-11-30 17:57:34 -0500

    comment out thank-you text to test heroku

--- app/controllers/twilio_controller.rb
+++ app/controllers/twilio_controller.rb
@@ -17,26 +17,26 @@ class TwilioController < ApplicationController
       resp.Text "Thanks for telling us what you love about Kate!."
     end
     
-    Message.create!(body: body, from: sender, city: city, state: state)
-    
-    thanks(sender)
+    # Message.create!(body: body, from: sender, city: city, state: state)
+    #
+    # thanks(sender)
     
     SMSLogger.log_text_message body, city
   end
 
   protected
 
-  def thanks(sender)
-    sid = ENV["TWILIO_SID"]
-    token = ENV["TWILIO_AUTH_TOKEN"]
-    twilio_number = "7328565344"
-    
-    @client = Twilio::REST::Client.new sid, token
-    
-    @client.account.sms.messages.create(
-      :from => "+1#{twilio_number}"),
-      :to => sender
-      :body => "Thanks for sharing what you love about Kate =)"
-    )
-  end
+  # def thanks(sender)
+  #   sid = ENV["TWILIO_SID"]
+  #   token = ENV["TWILIO_AUTH_TOKEN"]
+  #   twilio_number = "7328565344"
+  #
+  #   @client = Twilio::REST::Client.new sid, token
+  #
+  #   @client.account.sms.messages.create(
+  #     :from => "+1#{twilio_number}"),
+  #     :to => sender
+  #     :body => "Thanks for sharing what you love about Kate =)"
+  #   )
+  # end
 end
