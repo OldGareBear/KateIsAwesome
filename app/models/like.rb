1b21fc47e43311446f7e7df8a01cc035bc5680da
Gary Bethea <betheagary@gmail.com>
2014-12-03 12:03:53 -0500

    establish like-message association

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,3 +1,5 @@
 class Like < ActiveRecord::Base
   validates :message_id, :liker, presence: true
+  
+  belongs_to :message
 end
--- app/models/message.rb
+++ app/models/message.rb
@@ -1,3 +1,5 @@
 class Message < ActiveRecord::Base
   validates :body, null: false
+  
+  has_many :likes
 end
