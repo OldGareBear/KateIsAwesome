bba87a540d3e2f20a12ffba830dcd7ee2e0376f8
Gary Bethea <betheagary@gmail.com>
2014-12-08 09:32:38 -0500

    take image_path helper out of messages controller

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,7 +2,6 @@ class MessagesController < ApplicationController
   def index
     @messages = Message.order(created_at: :desc).page(params[:page]).per_page(10)
     @app_key = ENV['PUSHER_KEY']
-    @buttsit = image_path("buttsit.png") # not making pusher live yet
     
     render :index
   end
